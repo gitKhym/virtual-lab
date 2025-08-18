b77701cf08a0a144f52a1ec75b17b16abbb3c673
gitKhym <khayam.alpay@gmail.com>
2025-08-19 04:28:54 +1000

    Feat(hover): Hovering on items makes it floaty

--- scenes/simulations/ppe_simulation/ppe_selection.gd
+++ scenes/simulations/ppe_simulation/ppe_selection.gd
@@ -4,13 +4,38 @@ extends Area2D
 @export var item_name: String
 @export var description: String
 
-# Get the UI node directly from the scene tree
+@onready var sprite = $Sprite
 @onready var Ui = %UI
 
+@export var float_distance: float = 3.5
+@export var float_speed: float = 10.0
+
+var original_position: Vector2
+var floating = false
+var float_direction = 1.0
+var offset = 0.0
+
+
+func _ready():
+	original_position = sprite.position
+
+func _process(delta):
+	if floating:
+		offset += float_direction * float_speed * delta
+		if offset > float_distance:
+			offset = float_distance
+			float_direction = -1
+		elif offset < -float_distance:
+			offset = -float_distance
+			float_direction = 1
+		sprite.position = original_position + Vector2(0, -offset)
+	else:
+		sprite.position = original_position
 
 func _on_mouse_entered():
 	Ui.update_text(item_name, description)
+	floating = true
 
 
 func _on_mouse_exited():
-	Ui.clear_text()
+	floating = false
